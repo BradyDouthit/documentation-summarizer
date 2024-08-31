76c4374de16b771b9a3966b4584c1a815d118933
BradyDouthit <brady.douthit99@gmail.com>
2024-08-31 13:28:04 -0400

    init svelte repo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  export const name = "Brady";
+</script>
+
+<main>
+  <h1>Document Summarizer</h1>
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+</main>
+
+<style>
+  main {
+    background: red;
+  }
+</style>
