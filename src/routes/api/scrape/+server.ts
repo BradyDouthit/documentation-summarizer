43ed0b50becc39b443baacc64a58006261421140
BradyDouthit <brady.douthit99@gmail.com>
2024-08-31 14:59:20 -0400

    fetch urls

--- src/routes/api/scrape/+server.ts
+++ src/routes/api/scrape/+server.ts
@@ -1,19 +1,15 @@
 import type { RequestHandler } from "@sveltejs/kit";
-import { json } from "@sveltejs/kit";
+import { error, json } from "@sveltejs/kit";
 
 export const POST: RequestHandler = async ({ request }) => {
   const { url } = await request.json();
-  console.log(url);
-  // console.time("Starting browser");
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // console.timeEnd("Starting browser");
-  //
-  // console.time("Getting Page");
-  // await page.goto(params.url);
-  // const content = await page.content();
-  // console.log(content);
-  // console.timeEnd("Getting Page");
+  const resp = await fetch(url);
 
-  return json({ content: "" });
+  if (resp.status === 200) {
+    const text = await resp.text();
+    console.log(text);
+    return json({ content: "yo" });
+  }
+
+  return error(resp.status, "Something unexpected happened");
 };
