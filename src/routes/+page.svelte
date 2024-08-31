ad21e15b298e91aec36420e551c4acca51415c0e
BradyDouthit <brady.douthit99@gmail.com>
2024-08-31 13:54:37 -0400

    add url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,54 @@
 <script>
-  export const name = "Brady";
+  export const URL_INPUT_ID = "url-input";
+  let url = "";
+
+  $: {
+    console.log(url);
+  }
 </script>
 
 <main>
   <h1>Document Summarizer</h1>
-  <p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-  </p>
+  <div id={URL_INPUT_ID}>
+    <input
+      id="url-input"
+      type="text"
+      placeholder="https://go.dev/"
+      bind:value={url}
+    />
+  </div>
 </main>
 
 <style>
+  :global(html) {
+    width: 100%;
+    height: 100%;
+  }
+  :global(body) {
+    margin: 0;
+    padding: 0;
+
+    min-width: 100%;
+    width: 100%;
+    max-width: 100%;
+
+    min-height: 100%;
+    height: 100%;
+    max-height: 100%;
+  }
+
   main {
-    background: red;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  h1 {
+    width: fit-content;
+    height: fit-content;
+    margin: 0;
   }
 </style>
